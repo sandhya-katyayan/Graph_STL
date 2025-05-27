3a05143d579ff4dc39b70b17966298c3621ac39b
Sandhya Kumari <145265490+sandhya-katyayan@users.noreply.github.com>
2025-05-27 15:28:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,95 +1,147 @@
 #include <iostream>
-#include "Graph_Stl.hpp"
+#include "Graph.hpp"
 using namespace std;
-using namespace graphstl;
+int main(){
+  Graph g;
+  int n, m;
+  cout << "Enter the number of vertex and edges in the graph: ";
+  cin >> n >> m;
 
-int main() {
-    cout << "==== UNDIRECTED GRAPH TEST ====\n";
-    Graph<int> g1(7);  // 0 to 6
+  for (int i = 0; i < m; i++) {
+      int u, v, dir;
+      cin >> u >> v >> dir;
+      g.addEdge(u, v, dir);
+  }
 
-    g1.add_edge(0, 1);
-    g1.add_edge(0, 2);
-    g1.add_edge(1, 3);
-    g1.add_edge(1, 4);
-    g1.add_edge(2, 5);
-    g1.add_edge(4, 5);
-    g1.add_edge(3, 6);
+  cout << "Printing the adjacency list of the graph:\n";
+  g.PrintAdjList();
 
-    // DFS
-    cout << "DFS from 0: ";
-    for (int node : g1.dfs(0)) cout << node << " ";
-    cout << "\n";
+  cout<<"BFS traversal of the graph is: "<<endl;
+  vector<int>bfs=g.BFS(0);
+  for(auto i:bfs){
+    cout<<i<<"  ";
+  }
 
-    // BFS
-    cout << "BFS from 0: ";
-    for (int node : g1.bfs(0)) cout << node << " ";
-    cout << "\n";
+  cout<<endl;
 
-    // Cycle detection
-    cout << "Has Cycle: " << (g1.has_cycle() ? "Yes" : "No") << "\n";
+  cout<<"DFS traversal of the graph is: "<<endl;
+  vector<int>dfs=g.DFS(0,n);
+  for (auto it:dfs){
+    cout<<it<<" ";
+    
+  }
 
-    // Dijkstra
-    Graph<int> g2(true); // directed weighted graph
-    g2.add_edge(0, 1, 4);
-    g2.add_edge(0, 2, 1);
-    g2.add_edge(2, 1, 2);
-    g2.add_edge(1, 3, 1);
-    g2.add_edge(2, 3, 5);
+  cout<<endl<<endl;
 
-    cout << "\n==== DIJKSTRA TEST ====\n";
-    auto dist = g2.dijkstra(0);
-    for (auto& [node, d] : dist) {
-        cout << "Distance to " << node << ": " << d << "\n";
-    }
+  cout<<"Cycle detection in undirected graph using BFS:"<<endl;
+  if(g.detectCycleBFS(0,n)){
+    cout<<"Cycle is prsent in the graph"<<endl;
+  }
+  else 
+    cout<<"Cycle is not present int the graph"<<endl;
 
-    // Kruskal's MST
-    cout << "\n==== KRUSKAL MST TEST ====\n";
-    Graph<int> g3;
-    g3.add_edge(0, 1, 10);
-    g3.add_edge(0, 2, 6);
-    g3.add_edge(0, 3, 5);
-    g3.add_edge(1, 3, 15);
-    g3.add_edge(2, 3, 4);
+  cout<<endl;
 
-    auto mst = g3.kruskal_mst();
-    for (auto& [w, u, v] : mst) {
-        cout << u << " - " << v << " : " << w << "\n";
-    }
+  cout<<"Cycle detection in undirected graph using DFS:"<<endl;
+  if(g.detectCycleDfs(0, n)){
+    cout<<"Cycle is prsent in the graph"<<endl;
+  }
+  else{
+     cout<<"Cycle is not present int the graph"<<endl;
+  }
 
-    // Topological Sort
-    cout << "\n==== TOPOLOGICAL SORT TEST ====\n";
-    Graph<int> g4(true);
-    g4.add_edge(5, 2);
-    g4.add_edge(5, 0);
-    g4.add_edge(4, 0);
-    g4.add_edge(4, 1);
-    g4.add_edge(2, 3);
-    g4.add_edge(3, 1);
+  cout<<endl;
 
-    for (int node : g4.topological_sort())
-        cout << node << " ";
-    cout << "\n";
+  cout<<"checking wheather the graph is biparttiate or not using BFS:"<<endl;
 
-    // SCC
-    cout << "\n==== STRONGLY CONNECTED COMPONENTS TEST ====\n";
-    Graph<int> g5(8); // Vertices 0-7
-    g5.add_edge(0, 1);
-    g5.add_edge(1, 2);
-    g5.add_edge(2, 0);
-    g5.add_edge(2, 3);
-    g5.add_edge(3, 4);
-    g5.add_edge(4, 5);
-    g5.add_edge(5, 3);
-    g5.add_edge(6, 5);
-    g5.add_edge(6, 7);
+  if(g.IsGraphBipartites(n)){
+  cout<<"Graph is bipartite "<<endl;
+  }
 
-    auto scc_result = g5.scc();
-    int scc_id = 1;
-    for (auto& comp : scc_result) {
-        cout << "SCC " << scc_id++ << ": ";
-        for (int v : comp) cout << v << " ";
-        cout << "\n";
-    }
+  else{
+    cout<<"Graph is not bipartite"<<endl;
+  }
+  cout<<endl;
 
-    return 0;
+  cout<<"Cycle detection in directed graph using DFS:"<<endl;
+  if(g.iscycleDirected(n)){
+    cout<<"cycle is present in the graph "<<endl;
+  }
+  else {
+    cout<<"cycle is not present in the graph"<<endl;
+  }
+
+  cout<<endl;
+
+  cout<<"Topological sort of the graph using DFS:"<<endl;
+  vector<int>topo=g.toposort(n);
+
+  for(auto it:topo){
+    cout<<it<<" ";
+  }
+  cout<<endl;
+
+
+  cout<<"Topological sort of the graph using BFS:"<<endl;
+  vector<int>topoBFS=g.toposortBFS(n);
+
+
+  for(auto it:topoBFS){
+    cout<<it<<" ";
+  }
+  cout<<endl;
+  
+  
+
+  cout<<"Shortest path in directed acyclic graph using topological sort:"<<endl;
+
+  Graph g1;
+  int n1,m1;
+  cout<<"Emter the number of vertex and edges in the DAG:"<<endl;
+  cin>>n1>>m1;
+  int dir=1;
+  for(int i=0;i<m1;i++){
+    int u,v,weight;
+    cin>>u>>v>>weight;
+    g1.addEdgewithweight(u, v, weight,dir);
+  }
+
+  vector<int>shortestpath=g1.shortestpathdirected(n1, 0);
+  for(auto it:shortestpath){
+    cout<<it<<" ";
+  }
+  cout<<endl;
+
+  cout<<"shortest path in weighted graph using dijkstra algorigm : "<<endl;
+
+  vector<int>dijkstra=g1.dijkstra(n1, 0);
+
+  for(auto it:dijkstra){
+    cout<<it<<" ";
+  }
+
+  cout<<endl;
+
+  cout<<"shortest path in weighted graph using bellman ford algo:"<<endl;
+
+  vector<vector<int> >edges;
+  for(int i=0;i<m1;i++){
+    int u,v,weight;
+    cin>>u>>v>>weight;
+    edges.push_back({u,v,weight});
+    g1.addEdgewithweight(u, v, weight, dir);
+  }
+
+
+  vector<int>belmanford=g1.bellmanford(n1, 0 ,edges);
+
+  for(auto it:belmanford){
+    cout<<it<<" ";
+  }
+
+  cout<<endl;
+
+
+  return 0;
+  
 }
